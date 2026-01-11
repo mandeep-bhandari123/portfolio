71693c56cf697fc5b68a3167f65af639034110a8
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2026-01-11 17:44:53 +0545

    Final commit

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import gsap from 'gsap'
+import DrawSVGPlugin from 'gsap/DrawSVGPlugin'
 
 import { ReactComponent as OutlineSVG } from '../../../assets/images/file.svg'
 import './index.scss'
